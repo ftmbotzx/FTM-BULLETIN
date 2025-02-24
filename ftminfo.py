b2ee0409a79b031b41965fe520acba53a68f14ba
ftmbotzx <kumarasta91@gmail.com>
2025-02-24 17:11:32 +0530

    Create ftminfo.py

---
+++ ftminfo.py
@@ -0,0 +1,9 @@
+##### ftminfo.py (Configuration) #####
+
+# Fᴛᴍ Bᴜʟʟᴇᴛɪɴ Bot Configuration
+# Developed by @ftmdeveloperz - All Rights Reserved
+# Credits: @ftmdeveloperz | @ftmdeveloperz | @ftmdeveloperz
+
+TOKEN = ""  # Replace with your actual bot token
+OWNER_ID = 7744665378  # Replace with the owner's Telegram ID
+LOG_CHANNEL = -1002369984275  # Replace with your log channel ID
