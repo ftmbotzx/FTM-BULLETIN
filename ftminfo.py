677022192804b4582834cfafd78e953acdd7018b
ftmbotzx <kumarasta91@gmail.com>
2025-02-24 17:22:21 +0530

    Update ftminfo.py

--- ftminfo.py
+++ ftminfo.py
@@ -3,7 +3,8 @@
 # Fᴛᴍ Bᴜʟʟᴇᴛɪɴ Bot Configuration
 # Developed by @ftmdeveloperz - All Rights Reserved
 # Credits: @ftmdeveloperz | @ftmdeveloperz | @ftmdeveloperz
-
+API_ID =
+API_HASH = "b3a786dce1f4e7d56674b7cadfde3c9d" 
 TOKEN = ""  # Replace with your actual bot token
 OWNER_ID = 7744665378  # Replace with the owner's Telegram ID
 LOG_CHANNEL = -1002369984275  # Replace with your log channel ID
